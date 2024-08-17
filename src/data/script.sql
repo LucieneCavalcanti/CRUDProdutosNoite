28a9975f4126acbf651703bc8248379635313d07
LucieneCavalcanti <lucienecavalcanti@live.com>
2024-08-17 16:11:22 -0300

    script atualizado

--- src/data/script.sql
+++ src/data/script.sql
@@ -6,3 +6,17 @@ descricao varchar(50) not null,
 preco decimal(10,2) not null
 )
 select * from tbProdutos
+where preco>10 and preco<15
+
+select * from tbProdutos
+where descricao like 'Sabão%'
+
+insert into tbProdutos 
+(descricao, preco)
+values
+('Sabão Ipê',15.90)
+insert into tbProdutos 
+(descricao, preco)
+values
+('Sabão Omo',19.90),('Sabão Ulalá',14.6),
+('Macarrão Renata',4.66),('Miojo',1.99)
